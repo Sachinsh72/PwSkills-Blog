e6987d4bd1155ca415cc75157bd4bfe1fb0c391f
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-09-09 16:32:51 +0530

    create page

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-const createBlog = document.getElementById('create-blog').onclick = function() {
-    document.getElementById('createPage').style.display = "block"
-};
+const createBlog = document.getElementById('create-blog');
+const overlay = document.querySelector(".overlay");
+const createPage = document.querySelector('.createPage')
+const removeCreatePage = document.querySelector('.removecreatepage')
+
+createBlog.addEventListener("click", showoverlay)
+
+removeCreatePage.addEventListener("click", removeoverlay)
+
+// .onclick = function() {
+//     document.getElementById('createPage').style.display = "block"
+// };
+
+// console.log(overlay);
+
+// function showcreatepage(){
+    
+// }
+
+
+
+function showoverlay(){
+    overlay.classList.add('showoverlay')
+    createPage.classList.add('showCreatePage')
+}
+
+function removeoverlay(){
+    overlay.classList.remove('showoverlay')
+    createPage.classList.remove('showCreatePage')
+}
